20d06ecfb4a9763161d859f7f6abd26180feed22
Karst001 <ad.karsten@outlook.com>
2025-04-10 16:29:28 +0200

    boilerplate

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log('Happy developing ✨')
+
+//git steps
+//created new project
+//git status:
+// On branch master
+//
+// No commits yet
+// new file:   .idea/.gitignore
+// new file:   .idea/vcs.xml
+// new file:   index.js
+// new file:   package.json
+//
+// Changes not staged for commit:
+// (use "git add <file>..." to update what will be committed)
+// (use "git restore <file>..." to discard changes in working directory)
+// modified:   package.json
+
+//git add .
+
